634f5cbdb091dc0d0419362a31f63f19821dadcc
tom830001 <36037826+tom830001@users.noreply.github.com>
2020-05-25 10:01:31 +0800

    Update Signal.c

--- Signal.c
+++ Signal.c
@@ -1,16 +1,16 @@
 #include <stdio.h>	//standar input
-#include <signal.h> //signal
+#include <signal.h> 	//signal
 #include <stdlib.h>	//exit
 void signal_handler(int signum)
 {
 	printf("Get signal %d\n",signum);
-	exit(0);								//normal exit
+	exit(0);					//normal exit
 }
 
 void init_signal()
 {
 	struct sigaction sa_usr;
-	sa_usr.sa_flags = 0; 					// if handler exist set 0
+	sa_usr.sa_flags = 0; 				// if handler exist set 0
 	sa_usr.sa_handler = signal_handler;		//handler will start if catch signal below
 	sigaction(SIGINT,&sa_usr,NULL);			//Ctrl C
 	sigaction(SIGTSTP,&sa_usr,NULL);		//Ctrl Z
